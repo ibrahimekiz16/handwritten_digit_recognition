31fb41fa9e9a826da55cf9c998373b21bcd91294
Ibrahım Ekız <86263666+ibrahimekiz16@users.noreply.github.com>
2025-08-13 11:48:38 +0300

    Update train_mnist_model.py

--- train_mnist_model.py
+++ train_mnist_model.py
@@ -4,7 +4,6 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.utils import to_categorical
 
-# MNIST veri seti
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = x_train / 255.0
@@ -13,7 +12,6 @@ x_test = x_test / 255.0
 y_train = to_categorical(y_train, 10)
 y_test = to_categorical(y_test, 10)
 
-# Model oluştur
 model = Sequential([
     Flatten(input_shape=(28,28)),
     Dense(128, activation='relu'),
@@ -22,9 +20,7 @@ model = Sequential([
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Modeli eğit
 model.fit(x_train, y_train, epochs=5, batch_size=32, validation_data=(x_test, y_test))
 
-# Kaydet
 model.save("mnist_model.h5")
 print("Model kaydedildi: mnist_model.h5")
